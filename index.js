84a2aa3726bdffbb8fcb6cbb4f6685b0e3a5b922
Marvis Dan <marvis.dan@gmail.com>
2018-05-12 00:35:32 +0200

    node server done

---
+++ index.js
@@ -0,0 +1,26 @@
+// main starting point 
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost/auth');
+// App Setup
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+router(app);
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port);
+console.log('Server listening on:', port);
